d948045bd1f3654f38a5cbb0473ac03667f428c5
HMD <kamen888.md@gmail.com>
2020-02-18 17:33:25 +0700

    change folder

--- BookCar/LoginController/LoginController.m
+++ BookCar/LoginController/LoginController.m
@@ -6,19 +6,18 @@
 //  Copyright © 2020 hmduc. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "LoginController.h"
 #import "NavigationBarController.h"
-@interface ViewController ()
+@interface LoginController ()
 
 @end
 
-@implementation ViewController
+@implementation LoginController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     UINavigationBar *navigation = [self.navigationController navigationBar];
-    [navigation setBarTintColor:[UIColor colorWithRed:0 green:1 blue:0.2 alpha:0.5]];
 }
 
 @end
